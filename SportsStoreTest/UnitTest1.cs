9afaf7a6a967207ac84dab556bb69d5dae3d960b
Victor Avtandilyan <suren.vanyan@volo.global>
2021-03-09 17:59:59 +0400

    Add project files.

---
+++ SportsStoreTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SportsStoreTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
